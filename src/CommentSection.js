b1d01f05055027a9f5dc85a3e7c5ccadea254a3b
Kubon1999 <kuba.wolodzko@gmail.com>
2022-05-24 17:37:47 +0200

    added comments

--- src/CommentSection.js
+++ src/CommentSection.js
@@ -1,5 +1,39 @@
+import { useContext, useState } from "react";
+import AuthenticationContext from "./AuthenticationContext";
+import "./Video.css";
+
 const CommentSection = () => {
-  return <h1>COMMENTS:</h1>;
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
+  const userCredential = useContext(AuthenticationContext);
+
+  return (
+    <>
+      <h1>COMMENTS:</h1>
+      <label htmlFor="your-comment">Your comment:</label>
+      <input
+        id="your-comment"
+        value={comment}
+        placeholder="Your text here"
+        onChange={(e) => setComment(e.target.value)}
+      />
+
+      <button
+        className="comment-button"
+        onClick={() => {
+          if (comment !== "")
+            setComments(
+              comments.concat(userCredential.email + ":\n " + comment)
+            );
+        }}
+      >
+        ADD
+      </button>
+      {comments.map((comment, id) => {
+        return <p key={id}> {comment}</p>;
+      })}
+    </>
+  );
 };
 
 export default CommentSection;
